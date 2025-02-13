74c84b533696b13c7948f453e7ad55d9e5237a32
Jaclyn Sunardi <jsa411@sfu.ca>
2025-02-13 00:31:20 -0800

    added typed feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-//import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 import './App.css';
 
 function App() {
+  const homeRef = useRef(null);
+
+  useEffect(() => {
+    var typed = new Typed('.auto-type', {
+      strings: ['Third Year Student', 'Software Engineer', 'Entrepreneur'],
+      typeSpeed: 180,
+      backSpeed: 180,
+      loop: true,
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <div className = "HomePage">
-        <h1> Hi, I'm Jaclyn Sunardi!</h1>
-        
+      <div className="Header">
+        <div className="Logo-Brand">
+          <img src="logo.png" alt="Logo" id="Logo" />
+        </div>
       </div>
+      <section ref={homeRef}>
+        <div className="Content">
+          <div className="NameText">
+            <h1>Hi, I'm Jaclyn Sunardi!</h1>
+            <p><span className="auto-type"></span></p>
+          </div>
+          <div className="HomePagePhoto">
+            <img src="photo.png" alt="Profile" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
